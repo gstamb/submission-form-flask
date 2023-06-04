3c2ffb54e88b2ce3194ee44d604932bc7151a574
gstamb <georgi.stamboliyski@university.mentormate.com>
2023-06-05 01:46:42 +0200

    Added HTML-Flask connection, integrated Flask-SQLAlchemy for database, and implemented form data storage

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("PASSWORD")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
-@app.route('/')
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(90))
+    last_name = db.Column(db.String(90))
+    email = db.Column(db.String(90))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(90))
+
+
+@app.route('/', methods=["GET", "POST"])
 def index():
+    print(request.method)
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        available_date = request.form["date"]
+        date_obj = datetime.strptime(available_date, "%Y-%m-%d")
+        occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
+
     return render_template("index.html")
 
 
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
