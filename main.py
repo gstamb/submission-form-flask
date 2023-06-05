d5b9cee7af06128ed16e82a2c20f529cd7137d5a
gstamb <georgi.stamboliyski@university.mentormate.com>
2023-06-05 02:11:17 +0200

    Added send confirmation email upon submission

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("PASSWORD")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_PASSWORD"] = os.getenv("test_mail_server_password")
+app.config["MAIL_USERNAME"] = os.getenv("test_mail_server_username")
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -33,6 +41,18 @@ def index():
                     email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"Thank you for your submission, {first_name}. " \
+                       f"Here are your data: {first_name}\n{last_name}\n{date_obj}\n" \
+                       f"Thank you!"
+
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+
+        mail.send(message)
+
         flash("Your form was submitted successfully", "success")
 
     return render_template("index.html")
